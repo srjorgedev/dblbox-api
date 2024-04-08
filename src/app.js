4ed5b84a497a7c27d6dd43b783d6c8cd9f3f8bee
Jorge Sandoval <sandovl06roque@gmail.com>
2024-04-08 16:28:34 -0700

    Agregando el unitRoutes al app y cambiando la ruta de la api

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dataRoutes from './routes/dataRoutes.js'
+import unitRoutes from './routes/unitRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 9999
@@ -8,7 +9,8 @@ app.get('/', (req, res) => {
     res.send("<h1>Hola servidor</h1>")
 })
 
-app.use('/api', dataRoutes)
+app.use('/api/v1', dataRoutes)
+app.use('/api/v1', unitRoutes)
 
 app.listen(PORT, () => {
     console.log(`server listening on localhost:${PORT}`)
