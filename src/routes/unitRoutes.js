3ad8a548ff9dfa78401a6fd6bb6848e038cb0dc3
Jorge Sandoval <sandovl06roque@gmail.com>
2024-04-08 16:25:31 -0700

    Creando la ruta para subir unidades

---
+++ src/routes/unitRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const Router = express.Router()
+
+Router.post('/post/unit', async (req, res) => {
+    try {
+        const { basic, ability, arts, abilityZ, artsZ } = req.params
+
+        ////Continuar
+
+    } catch (error) {
+        res.json('Error')
+    }
+})
